45e51f9ed13218b2e5c7a1d2696c554f424c5385
ProcrastinatorDr <155884055+ProcrastinatorDr@users.noreply.github.com>
2024-01-13 21:51:49 +0300

    Added Movement
    Added movement in all directions with WASD keys. This was made using Input System with Player Input component. Result was incredibly short in code.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed = 0.1f;
+    private Vector3 directionVector;
+    public void OnMove(InputValue value)
     {
-        
+        directionVector = (Vector3)value.Get<Vector2>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x += 0.01f;
-        position.y += 0.01f;
-
-        transform.position = position;
-
+        transform.Translate(speed * directionVector);
     }
 }
